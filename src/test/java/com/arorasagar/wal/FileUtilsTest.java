ee557aba8f4c8d4f07ae9a52edc0ea1256ce4842
Sagar <arorasagar1811@gmail.com>
2023-01-16 00:06:56 -0800

    Added tests for file sort in FileUtils

--- src/test/java/com/arorasagar/wal/FileUtilsTest.java
+++ src/test/java/com/arorasagar/wal/FileUtilsTest.java
@@ -3,32 +3,11 @@ package com.arorasagar.wal;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 public class FileUtilsTest {
-
-    @Test
-    public void test1() throws IOException {
-        File file = Files.createTempFile(Paths.get(".").toAbsolutePath(), "wal-1234", ".log").toFile();
-        file.deleteOnExit();
-        Assert.assertEquals(1, FileUtils.listLogFiles(Paths.get(".")).size());
-    }
-
-    @Test
-    public void test() throws IOException {
-        File file1 = Files.createTempFile(Paths.get(".").toAbsolutePath(), "wal-", ".log").toFile();
-        file1.deleteOnExit();
-        File file2 = Files.createTempFile(Paths.get(".").toAbsolutePath(), "wal-", ".log").toFile();
-        file2.deleteOnExit();
-        File file3 = Files.createTempFile(Paths.get(".").toAbsolutePath(), "wal-", ".log").toFile();
-        file3.deleteOnExit();
-        Assert.assertEquals(1, FileUtils.listLogFiles(Paths.get(".")).size());
-    }
 
     @Test
     public void testFileSorting() throws IOException {
@@ -39,9 +18,8 @@ public class FileUtilsTest {
 
         List<FileMetadata> files = FileUtils.sortFilesByTimestamp(FileUtils.listLogFiles(directory.toAbsolutePath()));
 
-        for (FileMetadata fileMetadata : files) {
-            System.out.println(fileMetadata.toString());
-        }
-
+        Assert.assertEquals("wal-1235.log", files.get(0).getFilePath().getFileName().toString());
+        Assert.assertEquals("wal-234.log", files.get(1).getFilePath().getFileName().toString());
+        Assert.assertEquals("wal-123.log", files.get(2).getFilePath().getFileName().toString());
     }
 }
